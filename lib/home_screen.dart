d5a1b1631acb66b0a4c817fa713c1f4fa7170352
Phong03102003 <91301713+Phong03102003@users.noreply.github.com>
2024-03-13 18:36:06 +0700

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -59,6 +59,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Column(
           children: [
             Row(
